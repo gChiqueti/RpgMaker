306d2a1ec7e5a93f0ea932c0165461c41bff35d3
gChiqueti <36969020+gChiqueti@users.noreply.github.com>
2022-05-22 19:41:06 -0300

    Update scene_how_to_play.rb

--- scene_how_to_play.rb
+++ scene_how_to_play.rb
@@ -3,7 +3,7 @@
 # Free for commercial and non-commercial use / Livre para uso comercial e não-comercial
 # Credit not needed, but much appreciated / Não precisa dar créditos, mas se quiser pode =)
 # ------------------------------------------------
-# Insert this script above of others that change the title / Coloque esse script acima de outros que alteram a tela de título
+# Insert this script above others that change the title / Coloque esse script acima de outros que alteram a tela de título
 # ------------------------------------------------
 
 # Configurações
